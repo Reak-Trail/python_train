f81696a43b2d125c0ab5115be579df9d0843bc11
auleki <kodagiwa@gmail.com>
2020-04-04 08:35:51 +0400

    Train off

---
+++ products/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Product, Offer
+
+# This line of code adds the info saved to the db in the admin panel
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+class ProductAdmin(admin.ModelAdmin):
+     list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
+
